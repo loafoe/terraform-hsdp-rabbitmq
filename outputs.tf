8506064a3ce500f49dbd19fe4936a9d73bcb096b
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2020-11-02 16:21:58 +0100

    Match output with service key ones

--- outputs.tf
+++ outputs.tf
@@ -8,12 +8,32 @@ output "name_nodes" {
   value       = hsdp_container_host.rabbitmq.*.name
 }
 
-output "amqp_port" {
+output "port" {
   description = "RabbitMQ AMQP port"
   value       = "10000"
+}
+
+output "hostname" {
+  description = "RabbitMQ hostname"
+  value       = element(hsdp_container_host.rabbitmq.*.private_ip, 0)
 }
 
 output "admin_port" {
   description = "RabbitMQ Admin HTTP port"
   value       = "10001"
+}
+
+output "admin_username" {
+  description = "RabbitMQ admin username"
+  value       = "admin"
+}
+
+output "admin_password" {
+  description = "RabbitMQ admin password"
+  value       = random_password.password.result
+}
+
+output "dial" {
+  description = "AMQP Dial string"
+  value       = "amqp://${element(hsdp_container_host.rabbitmq.*.private_ip, 0)}:10000"
 }
